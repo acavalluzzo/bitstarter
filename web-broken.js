fe0cb51a55cb7737b0e3634b758d0ea2df7c0d41
Ubuntu <ubuntu@ip-172-31-35-131.us-west-2.compute.internal>
2013-07-10 20:36:47 +0000

    hw part 3 - added grader and checks, first vsns of each.

---
+++ web-broken.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var bitbuf = fs.readFile(index.html);
+
+var bitpage = bitbuf.toString("utf-8");
+
+app.get('/', function(request, response) {
+  response.write(bitpage);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
