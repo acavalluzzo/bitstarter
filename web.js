0cd73a1ae3b44f2538adbcefe0a03f59fbd216e4
Ubuntu <ubuntu@ip-172-31-35-131.us-west-2.compute.internal>
2013-07-10 20:04:31 +0000

    mucking with web.js for the coursera thingie.

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var bitbuf = fs.readFile(index.html);
 
 var bitpage = bitbuf.toString("utf-8");
