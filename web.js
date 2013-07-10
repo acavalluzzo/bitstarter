66b641822be65df4322a513f868e4d71546dc74b
Ubuntu <ubuntu@ip-172-31-35-131.us-west-2.compute.internal>
2013-07-10 19:34:39 +0000

    mucking with web.js for the coursera thingie.

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var bitpage = bitbuf.toString("utf-8")
 
 
 app.get('/', function(request, response) {
-  response.send(bitpage);
+  response.write(bitpage);
 });
 
 var port = process.env.PORT || 5000;
